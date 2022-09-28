3e216b24209a72885b9007190cda1a1d87600c6c
imran5740 <imranbsmrstucse@gmail.com>
2022-09-28 08:18:44 +0600

    New File Added

---
+++ WebApiDemo/WebApiDemo/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDemo.Controllers
+{
+    public class ProductController : ApiController
+    { static List<string> products = new List<string>()
+    {
+        "Laptop","DeskTop","Remote"
+    };
+        [HttpGet]
+        public List<string>GetAll()
+        {
+            return products;
+        }
+        [HttpGet]
+        public string GetProductNamebyID(int id)
+        {
+            return products[id];
+        }
+        [HttpGet]
+        public void Submit(string ProductName) 
+        {
+            products.Add(ProductName);
+        }
+    }
+}
